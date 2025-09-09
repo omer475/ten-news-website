321b3ec7b95e3070a1c5f96a3d1fa4ecbf20576e
omer475 <omer@tennews.ai>
2025-09-09 12:15:41 +0300

    Limit Today rotating titles to 20 characters with ellipsis

--- pages/index.js
+++ pages/index.js
@@ -850,7 +850,7 @@ export default function Home() {
                             'ENTERTAINMENT': '#ec4899'
                           };
                           const color = categoryColors[story.category] || '#3b82f6';
-                          const shortTitle = story.title.length > 15 ? story.title.substring(0, 15) + '...' : story.title;
+                          const shortTitle = story.title.length > 20 ? story.title.substring(0, 20) + '...' : story.title;
                           
                           return (
                             <span
