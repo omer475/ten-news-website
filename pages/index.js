64e6edb918ac8ae1538760cebdd54690f2e1c846
omer475 <omer@tennews.ai>
2025-09-26 20:52:46 +0100

    Set timeline height to 80px as requested

--- pages/index.js
+++ pages/index.js
@@ -1504,7 +1504,7 @@ export default function Home() {
                             <div style={{
                               display: 'flex',
                               width: '100%',
-                              height: '75px', // Optimal compact height
+                              height: '80px', // Fixed smaller height
                               overflow: 'hidden',
                               position: 'relative'
                             }}>
