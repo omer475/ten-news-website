41ce00c1c7fabc193d6ee053d325b5444ce1bcd9
omer475 <omer@tennews.ai>
2025-11-27 19:33:45 +0000

    Add PWA meta tags for iOS - black-translucent status bar

--- pages/_app.js
+++ pages/_app.js
@@ -82,6 +82,10 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#f5f5f7" />
       </Head>
       <ErrorBoundary>
         <Component {...pageProps} />
