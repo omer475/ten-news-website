183e9dc67da8c361feea3628699bd1b6b763ebc3
omer475 <omer@tennews.ai>
2025-11-28 18:19:19 +0000

    Move content much higher - increase padding to 58px for closer positioning

--- pages/index.js
+++ pages/index.js
@@ -4296,7 +4296,7 @@ export default function Home() {
                     {/* Content Area - Starts After Image */}
                     <div className="news-content" style={{
                       position: 'relative',
-                        paddingTop: 'calc(38vh + 52px)',
+                        paddingTop: 'calc(38vh + 58px)',
                         paddingLeft: '20px',
                         paddingRight: '20px',
                         zIndex: '2',
